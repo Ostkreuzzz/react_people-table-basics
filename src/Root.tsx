b984984cec7586def6aa044b4c22fb6acce2a67a
Michael Sulima <sulima719@gmail.com>
2024-10-29 12:59:53 +0200

    develop

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,19 +5,20 @@ import { App } from './App';
 import { HomePage } from './components/HomePage';
 import { PeoplePage } from './components/PeoplePage';
 import { NotFoundPage } from './components/NotFoundPage';
+import { Paths } from './utils/Paths';
 
 export const Root = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<App />}>
+      <Route path={Paths.ROOT} element={<App />}>
         <Route index element={<HomePage />} />
 
-        <Route path="people">
+        <Route path={Paths.PEOPLE}>
           <Route index element={<PeoplePage />} />
-          <Route path=":personSlug" element={<PeoplePage />} />
+          <Route path={Paths.PERSON_DETAILS} element={<PeoplePage />} />
         </Route>
 
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path={Paths.NOT_FOUND} element={<NotFoundPage />} />
       </Route>
     </Routes>
     <App />
